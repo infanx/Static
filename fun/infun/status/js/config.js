9115d0f2bf791b458cac651e6feee12816643133
InfantC <65985757+InfantC@users.noreply.github.com>
2022-01-01 23:10:47 +0800

    Add files via upload

--- fun/infun/status/js/config.js
+++ fun/infun/status/js/config.js
@@ -2,5 +2,5 @@ window.__PRE_CONFIG__ = {
   header: 'Server Status',
   subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
-  footer: '<p>Powered by <a href="">Server Status</a><br>Copyright © 2019 - 2021 By Infan</p>'
+  footer: '<p>Powered by <a href="">Server Status</a><br>Copyright © 2019 - 2022 By Infan</p>'
 };
